bd02ba22e4180c86e6ba19d95d32127052dbcd5d
Mazen Hindi <mazen.hindi@snowflake.com>
2025-07-28 14:07:50 +1000

    Add CBA migration and DDL extraction files with Git LFS
    - Added DMVA Sandpit migration procedure sample execution
    - Added comprehensive DDL extracts for CBA GDW migration:
      * Database, schema, table, view definitions (tracked with Git LFS)
      * Functions, procedures, macros, triggers
      * Join indexes and table structures
    - Added Teradata table migration procedure (P_MIGRATE_TERADATA_TABLE)
    - Added .gitignore to exclude system files
    - Configured Git LFS to handle large SQL files
    
    This commit includes all necessary DDL extracts and migration procedures for the CBA GDW project.

---
+++ GDW1/CBA_GDW_DDL_Extracts/DDL_Procedures.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fbd290c906d9a0c5063aef069fdec5e689b7db29a68914279422d392b1a81475
+size 39786357
---
+++ GDW1/CBA_GDW_DDL_Extracts/DDL_Tables1.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:32cf92457e373ecc30e827b07ea142e58b39d2d2c659be703f2b7f8616ea96c0
+size 180637551
