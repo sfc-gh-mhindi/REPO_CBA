0ad495c3b27d4894722b85bc481429c703578c3c
Mazen Hindi <mazen.hindi@snowflake.com>
2025-08-14 11:48:14 +1000

    NPW-DBT: refactor procedure to use JSON results and individual ResultSet/Cursor objects per step

--- GDW1/NPW-DBT/NPW DBT Project - Modified/models/cse_dataload/08extraction/cplbusapp/extpl_app/srcplappseq__extpl_app.sql
+++ GDW1/NPW-DBT/NPW DBT Project - Modified/models/cse_dataload/08extraction/cplbusapp/extpl_app/srcplappseq__extpl_app.sql
@@ -5,19 +5,20 @@ before_cte as(
 	from {{ ref('before__extpl_app') }}
 	where 1=2
 ),
-
+-- 20250813 - added by mhindi to use ingested table instead of the one in the source
 {% set default_inprocess =
     cvar('intermediate_db') ~ '.' ~ cvar('files_schema') ~ '.' ~ cvar('base_dir') ~
     '__INPROCESS__CSE_CPL_BUS_APP_' ~ cvar('run_stream') ~ '_' ~ cvar('etl_process_dt') ~ '__DLY'
 %}
 {% set inprocess_src = var('inprocess_source_override', default_inprocess) %}
+-- 20250813 - added by mhindi to use ingested table instead of the one in the source
 
 srcplappseq AS (
 	SELECT MOD_TIMESTAMP,
 		PL_APP_ID,
 		NOMINATED_BRANCH_ID,
 		PL_PACKAGE_CAT_ID
-	
+	-- 20250813 - added by mhindi to use ingested table instead of the one in the source
 	FROM {{ inprocess_src }}
 )
 
